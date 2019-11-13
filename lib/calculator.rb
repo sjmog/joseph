9703724bab9cb285fe08a5d7d3f5d892e795270e
sjmog <s_morgan@me.com>
2019-11-13 12:52:29 +0000

    Handles simple addition

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,16 @@
 class Calculator
   def self.calculate(expression)
-    [expression, 1]
+    stack = expression.split(" ")
+
+    case stack.length
+    when 0
+      evaluation = 0
+    when 1
+      evaluation = expression.to_i
+    else
+      evaluation = stack[0].to_i.send(stack[1], stack[2].to_i)
+    end
+
+    [expression, evaluation]
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,10 +3,24 @@ require 'calculator'
 RSpec.describe Calculator do
   describe '#calculate' do
     it 'handles a single number' do
-      expected = ["1", 1]
-      actual   = Calculator.calculate("1")
-      
-      expect(actual).to eq expected
+      input  = "1"
+      output = ["1", 1]
+
+      expect(Calculator.calculate(input)).to eq output
+    end
+
+    it 'handles an empty string' do
+      input  = ""
+      output = ["", 0]
+
+      expect(Calculator.calculate(input)).to eq output
+    end
+
+    it 'handles simple addition' do
+      input  = "1 + 1"
+      output = ["1 + 1", 2]
+
+      expect(Calculator.calculate(input)).to eq output
     end
   end
 end
